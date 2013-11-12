24f9937e249b460e452247939a362aec79091b6f
nrrichtr <nrrichtr@umich.edu>
2013-11-12 14:47:54 -0500

    Pre-presentation Changes
    Sorry for the branch, didn't want to overwrite master with stuff we
    didn't talk about. I'll email you guys the changelog.

--- SettingManager/gen/com/example/settingmanager/R.java
+++ SettingManager/gen/com/example/settingmanager/R.java
@@ -10,6 +10,10 @@ package com.example.settingmanager;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f060000;
+        public static final int white=0x7f060001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -24,32 +28,32 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080012;
-        public static final int bluetooth=0x7f08000d;
-        public static final int bluetoothCheckbox=0x7f080006;
-        public static final int checkBox2=0x7f080001;
-        public static final int create=0x7f080011;
-        public static final int createButton=0x7f080008;
-        public static final int loadButton=0x7f080009;
-        public static final int mobile=0x7f08000c;
-        public static final int mobileNetworkCheckBox=0x7f080002;
-        public static final int ring=0x7f08000e;
-        public static final int ringCheckbox=0x7f080000;
-        public static final int settingNameEditText=0x7f080007;
-        public static final int settingsName=0x7f08000a;
-        public static final int silent=0x7f080010;
-        public static final int silentCheckbox=0x7f080005;
-        public static final int vibrate=0x7f08000f;
-        public static final int vibrateCheckbox=0x7f080004;
-        public static final int wifi=0x7f08000b;
-        public static final int wifiCheckbox=0x7f080003;
+        public static final int action_settings=0x7f090012;
+        public static final int bluetooth=0x7f09000d;
+        public static final int create=0x7f090011;
+        public static final int mainCreate=0x7f090007;
+        public static final int mainLoad=0x7f090006;
+        public static final int mobile=0x7f09000c;
+        public static final int name=0x7f090008;
+        public static final int ring=0x7f09000e;
+        public static final int settingsName=0x7f09000a;
+        public static final int silent=0x7f090010;
+        public static final int spinner=0x7f090009;
+        public static final int switchBluetooth=0x7f090000;
+        public static final int switchMobile=0x7f090003;
+        public static final int switchRing=0x7f090001;
+        public static final int switchSilent=0x7f090005;
+        public static final int switchVibrate=0x7f090004;
+        public static final int switchWifi=0x7f090002;
+        public static final int vibrate=0x7f09000f;
+        public static final int wifi=0x7f09000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int settings=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -78,10 +82,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- SettingManager/src/com/example/settingmanager/MainActivity.java
+++ SettingManager/src/com/example/settingmanager/MainActivity.java
@@ -4,34 +4,36 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 import com.example.settingmanager.MainActivity;
 
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.media.AudioManager;
 import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.net.Uri;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothManager;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.AssetManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -39,16 +41,116 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		final CheckBox wifiCheckBox = (CheckBox) findViewById(R.id.wifiCheckbox);
-		final CheckBox blueToothCheckBox = (CheckBox) findViewById(R.id.bluetoothCheckbox);
-		final CheckBox mobileNetworkCheckBox = (CheckBox) findViewById(R.id.mobileNetworkCheckBox);
-		final CheckBox ringCheckBox = (CheckBox) findViewById(R.id.ringCheckbox);
-		final CheckBox vibrateCheckBox = (CheckBox) findViewById(R.id.vibrateCheckbox);
-		final CheckBox silentCheckBox = (CheckBox) findViewById(R.id.silentCheckbox);
-		final Button createButton = (Button) findViewById(R.id.createButton);
-		final Button loadButton = (Button) findViewById(R.id.loadButton);
-		final EditText settingName = (EditText) findViewById(R.id.settingNameEditText);
+		
+		final Switch wifiCheckBox = (Switch) findViewById(R.id.switchWifi);
+		final Switch blueToothCheckBox = (Switch) findViewById(R.id.switchBluetooth);
+		final Switch mobileNetworkCheckBox = (Switch) findViewById(R.id.switchMobile);
+		final Switch ringCheckBox = (Switch) findViewById(R.id.switchRing);
+		final Switch vibrateCheckBox = (Switch) findViewById(R.id.switchVibrate);
+		final Switch silentCheckBox = (Switch) findViewById(R.id.switchSilent);
+		final Button createButton = (Button) findViewById(R.id.mainCreate);
+		final Button loadButton = (Button) findViewById(R.id.mainLoad);
+		final EditText settingName = (EditText) findViewById(R.id.name);
 		final Context context = this;
+		final Spinner spinner = (Spinner) findViewById(R.id.spinner);
+		final ArrayList<String> spinnerArray = new ArrayList<String>();
+		File[] files = context.getFilesDir().listFiles();
+		spinnerArray.add("");
+		for (File file : files) {
+			if (!file.isDirectory()) {
+				spinnerArray.add(file.getName());
+				Log.d("File", file.getName());
+			}
+		}
+		spinner.setAdapter(new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, spinnerArray));
+		spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				((TextView)arg1).setText(null);
+				settingName.setText(spinner.getItemAtPosition(arg2).toString());
+				//visualize settings
+				try {
+					FileInputStream input_stream = openFileInput(settingName.getText().toString());
+					InputStreamReader input_stream_reader = new InputStreamReader(input_stream);
+					BufferedReader buffered_reader = new BufferedReader(input_stream_reader);
+					String received = "";
+					StringBuilder string_builder = new StringBuilder();
+					while ((received = buffered_reader.readLine()) != null)
+						string_builder.append(received);
+					input_stream.close();
+					String settings = string_builder.toString();
+					//Will change the BlueTooth settings
+					if (settings.substring(0, 1).equals("1"))
+						blueToothCheckBox.setChecked(true);
+					else
+						blueToothCheckBox.setChecked(false);
+					//Will change the WiFi settings
+					if (settings.substring(1,2).equals("1"))
+						wifiCheckBox.setChecked(true);
+					else
+						wifiCheckBox.setChecked(false);
+					//Will change the 3G settings
+					if (settings.substring(2,3).equals("1"))
+						mobileNetworkCheckBox.setChecked(true);
+					else
+						mobileNetworkCheckBox.setChecked(false);
+					if (settings.substring(3,4).equals("0"))
+						ringCheckBox.setChecked(true);
+					else if (settings.substring(3,4).equals("1"))
+						vibrateCheckBox.setChecked(true);
+					else if (settings.substring(3,4).equals("2"))
+						silentCheckBox.setChecked(true);
+				}
+				catch (Exception e) {
+					
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {}
+		});
+		
+		vibrateCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if(isChecked)
+				{
+					if(silentCheckBox.isChecked())
+						silentCheckBox.setChecked(false);
+					if(ringCheckBox.isChecked())
+						ringCheckBox.setChecked(false);
+				}
+			}
+		});
+		ringCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if(isChecked)
+				{
+					if(vibrateCheckBox.isChecked())
+						vibrateCheckBox.setChecked(false);
+					if(silentCheckBox.isChecked())
+						silentCheckBox.setChecked(false);
+				}
+			}
+		});
+		
+		silentCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if(isChecked)
+				{
+					if(vibrateCheckBox.isChecked())
+						vibrateCheckBox.setChecked(false);
+					if(ringCheckBox.isChecked())
+						ringCheckBox.setChecked(false);
+				}
+			}
+		});
 		
 		createButton.setOnClickListener(new View.OnClickListener() {
 			
@@ -101,6 +203,17 @@ public class MainActivity extends Activity {
 						
 					output_stream.write(settings.getBytes());
 					output_stream.close();
+					
+					spinnerArray.clear();
+					spinnerArray.add("");
+					File[] files = context.getFilesDir().listFiles();
+					for (File inFile : files) {
+						if (!inFile.isDirectory()) {
+							spinnerArray.add(inFile.getName());
+							Log.d("File", inFile.getName());
+						}
+					}
+					spinner.setAdapter(new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, spinnerArray));
 				}
 				catch (Exception e)
 				{
@@ -114,17 +227,11 @@ public class MainActivity extends Activity {
 				
 			@Override
 			public void onClick(View v) {
-                AudioManager audiomanager =(AudioManager)MainActivity.this.getSystemService(Context.AUDIO_SERVICE);
-            	BluetoothAdapter blueTooth = BluetoothAdapter.getDefaultAdapter();  
+				AudioManager audiomanager =(AudioManager)MainActivity.this.getSystemService(Context.AUDIO_SERVICE);
+				BluetoothAdapter blueTooth = BluetoothAdapter.getDefaultAdapter();  
 				WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-                String settings = "";
-                Boolean bluetoothState = false;
-                Boolean wifiState = false;
-                Boolean mobileNetworkState = false;
-                Boolean ringState = false;
-                Boolean vibrateState = false;
-                Boolean silentState = false;
-                
+				String settings = "";
+				
 				//File file = new File(context.getFilesDir(), settingName.getText().toString());
 				//String temp_string = "testing 1 2 3";
 				//FileOutputStream output_stream;
@@ -165,10 +272,10 @@ public class MainActivity extends Activity {
 					{
 						wifi.setWifiEnabled(false);
 					}
-	                //Will change the 3G settings
+					//Will change the 3G settings
 					if (settings.substring(2,3).equals("1"))
 					{
-		            	try
+						try
 						{		
 							 ConnectivityManager conman = (ConnectivityManager) context.getSystemService(CONNECTIVITY_SERVICE);   							   
 							 Class conmanClass = Class.forName(conman.getClass().getName());   
@@ -186,7 +293,7 @@ public class MainActivity extends Activity {
 					}
 					else
 					{
-		            	try
+						try
 						{		
 							 ConnectivityManager conman = (ConnectivityManager) context.getSystemService(CONNECTIVITY_SERVICE);   							   
 							 Class conmanClass = Class.forName(conman.getClass().getName());   
@@ -214,44 +321,38 @@ public class MainActivity extends Activity {
 					{
 						audiomanager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
 					}
+						
 					
 				}
 				catch (Exception e)
 				{
 					e.printStackTrace();
 				}
-                
-                /*
-                String filename = settingName.getText().toString();
-                FileOutputStream outputStream;
-                try
-                {
-                	outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
-                	outputStream.write("hello".getBytes());
-                	outputStream.close();
-                }
-                catch (Exception e)
-                {
-                	
-                }
-                */
-                
-        		SharedPreferences sharedPref = getSharedPreferences("settings", 0);
-        		SharedPreferences.Editor editor = sharedPref.edit();
-        		editor.putBoolean("wifi", wifiCheckBox.isChecked());
-        		editor.putBoolean("bluetooth", blueToothCheckBox.isChecked());
-        		editor.putBoolean("mobile", mobileNetworkCheckBox.isChecked());
-        		editor.putBoolean("ring", ringCheckBox.isChecked());
-        		editor.putBoolean("vibrate", vibrateCheckBox.isChecked());
-        		editor.putBoolean("silent", silentCheckBox.isChecked());
-        		editor.commit();
-        		
-        		Boolean wifiTemp = sharedPref.getBoolean("wifi", false);
-        		Boolean bluetooth = sharedPref.getBoolean("bluetooth", false);
-        		Boolean mobile = sharedPref.getBoolean("mobile", false);
-        		Boolean ring = sharedPref.getBoolean("ring", false);
-        		Boolean vibrate = sharedPref.getBoolean("vibrate", false);
-        		Boolean silent = sharedPref.getBoolean("silent", false);
+				
+				/*
+				String filename = settingName.getText().toString();
+				FileOutputStream outputStream;
+				try
+				{
+					outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+					outputStream.write("hello".getBytes());
+					outputStream.close();
+				}
+				catch (Exception e)
+				{
+					
+				}
+				*/
+				
+				SharedPreferences sharedPref = getSharedPreferences("settings", 0);
+				SharedPreferences.Editor editor = sharedPref.edit();
+				editor.putBoolean("wifi", wifiCheckBox.isChecked());
+				editor.putBoolean("bluetooth", blueToothCheckBox.isChecked());
+				editor.putBoolean("mobile", mobileNetworkCheckBox.isChecked());
+				editor.putBoolean("ring", ringCheckBox.isChecked());
+				editor.putBoolean("vibrate", vibrateCheckBox.isChecked());
+				editor.putBoolean("silent", silentCheckBox.isChecked());
+				editor.commit();
 			}
 		});
 	}
@@ -262,5 +363,4 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
 }
